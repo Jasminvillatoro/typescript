44da2dbc31a30b8dbbf17f4b5b1a527a7a41ce2b
Jasmin Villatoro <Jasminvilla321@gmail.com>
2022-11-10 17:57:13 -0500

    Types folder

---
+++ Types/variables.ts
@@ -0,0 +1,9 @@
+// TypeScript Annotation Basics
+
+// Variable Types 
+/* Assigning a basic type to a variable is easy just add :Type after the variable name */
+
+//EX
+const myAwesomeVariable: string = 'So Awesome';
+
+let movieTitle: string = 'Spiderman';
